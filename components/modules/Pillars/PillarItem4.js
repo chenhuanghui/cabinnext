ac7a17e35aa3bd31932015605c914c8e050a54cb
enZo <inbox.huytran@gmail.com>
2020-05-18 14:14:34 +0700

    update style pillar4

--- components/modules/Pillars/PillarItem4.js
+++ components/modules/Pillars/PillarItem4.js
@@ -1,28 +1,25 @@
 const PillarItem4 = props => (
-    <div id="Sell" className="pillars__wrapper">
-        <div className="pillars__image">
-            <div className="demo-video js-demo-video--loading pillars-animation pillars-animation__sell hide--mobile">
-                <div className="demo-video__video-container">
-                    <img src={props.dataItem4.pil_block4_img_src}/>
-                </div>                        
-            </div>
-
-            <div className="demo-video js-demo-video--loading pillars-animation pillars-animation__sell display--mobile">
-                <div className="demo-video__video-container">
-                    <img src={props.dataItem4.pil_block4_img_src}/>
-                </div>
-            </div>
-        </div>
-
-        <div className="block pillars__block pillars__block--sell">
+    <div id="Market Pillar4" className="pillars__wrapper gutter-bottom">
+        <div className="block pillars__block">
             <h3 className="block__heading heading--2">
                 {props.dataItem4.pil_block4_title}
             </h3>
             <p className="block__content">{props.dataItem4.pil_block4_desc}</p>
-            <a className="pillars__block-link styled-link" href={props.dataItem4.pil_block4_btn_href}>
+            <a className="pillars__block-link styled-link" href= {props.dataItem4.pil_block4_btn_href}>
                 {props.dataItem4.pil_block4_btn_title}
+                {/* <svg className="icon styled-link__icon" aria-hidden="true" focusable="false"><use xlink:href="#modules-arrow-right" /></svg> */}
             </a>
         </div>
+        <span className="image lazyload-image pillars__image pillars__image--market" >
+            <span className="lazyload-image__placeholder">
+                <img src={props.dataItem4.pil_block4_img_src}/>
+            </span>
+        </span>
+
+        <style jsx>{`
+                .image.lazyload-image pillars__image.pillars__image--market{max-width: 681px; max-height: 547px;}
+                .lazyload-image__placeholder{padding-bottom: 80.3231%;}
+        `}</style>
     </div>
 );
 export default PillarItem4;    
