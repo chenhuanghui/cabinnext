8552b27d15c6c6af2069a08f47259bd1db3f75cc
enZo <inbox.huytran@gmail.com>
2020-05-19 10:56:14 +0700

    convert explore to location

--- pages/explore.js
+++
@@ -1,7 +0,0 @@
-import LayoutExplore from '../components/LayoutExplore'
-
-const Explore = props => (
-    <LayoutExplore />
-);
-  
-export default Explore;
---
+++ pages/location.js
@@ -0,0 +1,7 @@
+import LayoutExploreLocation from '../components/LayoutExploreLocation'
+
+const Location = props => (
+    <LayoutExploreLocation />
+);
+  
+export default Location;
