9a719142339c41a95a17af25886243dbb34da0ab
enZo <inbox.huytran@gmail.com>
2020-05-27 16:34:27 +0700

    add section1 setup

--- components/LayoutSetupStore.js
+++ components/LayoutSetupStore.js
@@ -2,12 +2,70 @@ import Head from 'next/head'
 import React from 'react';
 
 import Header from './Header'
+import Announcement from './sections/Announcement';
+import SetupSection1 from './sections/Setup/Section1';
 import Footer from './Footer'
 
 export default class LayoutSetupStore extends React.Component { 
+    constructor(props){
+        super(props);
+    
+        this.state = {
+            announcementData: []
+        }
+      }
+
+    componentDidMount () {
+        let currentComponent = this;
+        var Airtable = require('airtable');
+        var base = new Airtable({apiKey: 'keyLNupG6zOmmokND'}).base('appZ1bpUbqpieMgfe');
+
+        base('Announcement').find('rech8uFdriXPNBETt', function(err, record) {
+            if (err) { console.error(err); return; }
+            currentComponent.setState({ announcementData: record.fields })
+        });
+    }
+
     render (){
+        const { announcementData} = this.state;
         return (
-            <p>setup store</p>
+            <div className="layout">
+                <Head>
+                    <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
+                    <meta name="author" content="CabinFood" />
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
+                    <link rel="stylesheet" media="screen" href="https://cdn.shopify.com/assets2/manifests/pillar/start-367dd49db372536352ca153b1f287737ea503ed01e842a0f65d7a2d0a0b082a1.css" />
+                    <link rel="shortcut icon" type="image/png" href="https://cdn.shopify.com/shopify-marketing_assets/static/shopify-favicon.png" />
+                    <title>CabinFood | easy to Start, Fast to grow</title>
+                </Head>
+
+                <div className="page--home">
+                    <Announcement announcementData={announcementData}/>
+
+                    {/* <!-- Header */}
+                    <Header />
+                    <div id="PageContainer">
+                        <main id="Main">
+                            <SetupSection1 />
+                            
+                            <div className="grid">
+                                <div className="grid__item back-to-top-link-wrapper">
+                                    <a className="back-to-top-link link-scroll-to styled-link styled-link--skin-inherit" href="#PageContainer">
+                                        Back to top
+                                        <svg className="icon styled-link__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path d="M17.707 9.293l-5-5c-.39-.39-1.023-.39-1.414 0s-.39 1.023 0 1.414L14.586 9H3c-.553 0-1 .448-1 1s.447 1 1 1h11.586l-3.293 3.293c-.39.39-.39 1.023 0 1.414.195.195.45.293.707.293s.512-.098.707-.293l5-5c.39-.39.39-1.023 0-1.414z"/>
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
+                        </main>
+                        <Footer />
+                    </div>
+                </div>
+                <style jsx>{`
+                    .marketing-nav.marketing-nav--solid {background-color: #fbf7ed !important;}
+                `}</style>
+            </div>
         )
     }
 }
