f498b847ce658e2559a5b57a15ea6aed6cdaafc5
enZo <inbox.huytran@gmail.com>
2020-07-10 12:14:05 +0700

    build test intercom

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* <script src="/static/intercom.js" /> */}
+          <script src="/static/intercom.js" />
           <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/7453021.js"></script>
         </Head>
         <body>
